0c82f0fab9804ae8fcba2220b14a72d44ab7695c
Yesulen Tsedensodnom <sodesulen@gmail.com>
2025-01-30 15:24:55 +0000

    added hyperlinks between about and index pages

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,6 +21,5 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('admin/', admin.site.urls),
-    path('about/', views.about, name = 'index')
+    path('admin/', admin.site.urls)
 ]
