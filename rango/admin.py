b1dec50815e04b6e29ab460ea03c13bc307b334c
Yesulen Tsedensodnom <sodesulen@gmail.com>
2025-02-11 21:13:16 +0000

    completed chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -4,9 +4,12 @@ from .models import Page
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
-# admin.site.register(Page)
 admin.site.register(Page, PageAdmin)
